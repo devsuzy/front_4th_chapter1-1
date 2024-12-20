4249dddf807183a57a99dad0dc160092fca268cf
suzy <susie0529@naver.com>
2024-12-20 10:49:32 +0900

    feat: 심화과제 2번 통과

--- src/util/router.js
+++ src/util/router.js
@@ -4,60 +4,63 @@ import LoginPage from "../pages/LoginPage";
 import ErrorPage from "../pages/ErrorPage";
 
 class Router {
-    constructor() {
-        this.setRoute();
-        this.isLoggedIn = !!localStorage.getItem("user");
-        window.addEventListener("popstate", this.handlePopState.bind(this));
-    }
+  constructor() {
+    this.setRoute();
+    this.isLoggedIn = !!localStorage.getItem("user");
+    window.addEventListener("popstate", this.handlePopState.bind(this));
+  }
 
-    setRoute() {
-        this.isLoggedIn = !!localStorage.getItem("user");
-        this.routes = [
-            { path: "/", page: MainPage },
-            { path: "/profile", page: ProfilePage },
-            { path: "/login",  page: LoginPage}
-        ]
-    }
+  setRoute() {
+    this.isLoggedIn = !!localStorage.getItem("user");
+    this.routes = [
+      { path: "/", page: MainPage },
+      { path: "/profile", page: ProfilePage },
+      { path: "/login", page: LoginPage },
+    ];
+  }
 
-    addRoute(path, handler) {
-        this.routes[path] = handler;
-    }
+  addRoute(path, handler) {
+    this.routes[path] = handler;
+  }
 
-    navigateTo(path) {        
-        history.pushState(null, null, path);
-        this.handleRoute(path);
-    }
+  navigateTo(path) {
+    history.pushState(null, null, path);
+    this.handleRoute(path);
+  }
 
-    handlePopState() {
-        this.handleRoute(window.location.pathname);
-    }
+  handlePopState() {
+    this.handleRoute(window.location.pathname);
+  }
 
-    handleRoute(path) {
-        const route = this.routes.find((route) => route.path === path);
-        const $root = document.querySelector("#root");
+  handleRoute(path) {
+    const route = this.routes.find((route) => route.path === path);
+    const $root = document.querySelector("#root");
 
-        if (path === "/profile" && !localStorage.getItem("user")) {
-            this.navigateTo("/login");
-            return;
-        } else if (path === "/logout") {
-            this.handleLogout();
-            return;
-        } else if (route && route.page) {
-            new route.page($root);
-        } else {
-            new ErrorPage($root);
-        }
+    if (path === "/profile" && !localStorage.getItem("user")) {
+      this.navigateTo("/login");
+      return;
+    } else if (path === "/login" && localStorage.getItem("user")) {
+      this.navigateTo("/");
+      return;
+    } else if (path === "/logout") {
+      this.handleLogout();
+      return;
+    } else if (route && route.page) {
+      new route.page($root);
+    } else {
+      new ErrorPage($root);
     }
+  }
 
-    handleLogout() {
-        localStorage.removeItem("user");  
-        this.setRoute(); 
-        this.navigateTo("/login");
-    }
+  handleLogout() {
+    localStorage.removeItem("user");
+    this.setRoute();
+    this.navigateTo("/login");
+  }
 
-    getRoutes() {
-        return this.routes;
-    }
+  getRoutes() {
+    return this.routes;
+  }
 }
 
 export default Router;
